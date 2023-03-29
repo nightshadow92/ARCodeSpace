fee30d4148a63c013ad76a1c2600f5e198d2aa95
nightshadow92 <41315829+nightshadow92@users.noreply.github.com>
2023-03-29 14:28:24 -0500

    Update install-tools.sh

--- install-tools.sh
+++ install-tools.sh
@@ -1,10 +1,10 @@
-sudo apt update
+apt update
 wget https://mega.nz/linux/repo/xUbuntu_20.04/amd64/megacmd-xUbuntu_20.04_amd64.deb
-sudo apt install ./megacmd*.deb
-sudo rm ./megacmd*.deb
-sudo apt install npm
-sudo npm i xdccjs
+apt install ./megacmd*.deb
+rm ./megacmd*.deb
+apt install npm
+npm i xdccjs
 
 
-sudo apt install openjdk-8-jre-headless
+apt install openjdk-8-jre-headless
 wget http://installer.jdownloader.org/JDownloader.jar

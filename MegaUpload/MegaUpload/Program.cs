199f5b3f697c644af92a2087b01b973b7bacbf7c
nightshadow92 <s.allen.hare92@gmail.com>
2023-03-29 16:34:58 -0500

    created program to upload to mega.nz

---
+++ MegaUpload/MegaUpload/Program.cs
@@ -0,0 +1,37 @@
+﻿#r "nuget:MegaApiClient/1.10.3"
+using CG.Web.MegaApiClient;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+void DisplayNodesRecursive (IEnumerable<INode> nodes, INode parent, int level = 0) {
+    IEnumerable<INode> children = nodes.Where(x => x.ParentId == parent.Id);
+    foreach (INode child in children) {
+        Console.WriteLine(child.Name);
+        if (child.Type == NodeType.Directory) {
+            DisplayNodesRecursive(nodes, child, level + 1);
+        }
+    }
+}
+
+string authjson = System.Environment.GetEnvironmentVariable("MEGA_AUTH");
+var login = JsonSerializer.Deserialize<MegaApiClient.AuthInfos>(authjson);
+
+MegaApiClient client = new MegaApiClient();
+client.Login(login);
+
+
+//IEnumerable<INode> nodes = client.GetNodes();
+//INode node = nodes.Single(x => x.Type == NodeType.Root);
+//client.UploadFile(@"log.txt", node);
+
+
+
+
+
+
+IEnumerable<INode> nodes = client.GetNodes();
+
+INode parent = nodes.Single(n => n.Type == NodeType.Root);
+DisplayNodesRecursive(nodes, parent);
+
+client.Logout();

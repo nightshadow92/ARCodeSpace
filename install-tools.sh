21df7de3e3299b435a0c9dbc6fa540306b4b3b93
nightshadow92 <41315829+nightshadow92@users.noreply.github.com>
2023-04-02 17:55:59 +0000

    add pwsh to install script

--- install-tools.sh
+++ install-tools.sh
@@ -9,3 +9,12 @@ npm i xdccjs
 dotnet publish  MegaUpload/MegaUpload.sln --sc true /p:PublishSingleFile=true -o mega
 apt install openjdk-8-jre-headless -y
 wget http://installer.jdownloader.org/JDownloader.jar
+
+
+
+sudo apt-get install -y wget apt-transport-https software-properties-common
+wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install -y powershell

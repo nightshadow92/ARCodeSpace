5e01eae90b6923233bfce987f333b5819684296e
nightshadow92 <41315829+nightshadow92@users.noreply.github.com>
2023-05-15 12:35:05 -0500

    Update install-tools.sh

--- install-tools.sh
+++ install-tools.sh
@@ -1,25 +1,25 @@
-apt update
-apt upgrade -y
-apt install npm wget apt-transport-https software-properties-common -y
-npm i xdccjs
-git clone https://github.com/nightshadow92/MegaUpload.git
-
-
-dotnet publish  MegaUpload/MegaUpload.sln --sc true /p:PublishSingleFile=true -o mega
-
-
 wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
-sudo apt update
-sudo apt install -y powershell
-#pwsh Download.ps1
+apt update
+apt upgrade -y
+apt install npm wget apt-transport-https software-properties-common powershell openjdk-8-jre-headless -y
+npm i xdccjs
 
+
+git clone https://github.com/nightshadow92/MegaUpload.git
+dotnet publish  MegaUpload/MegaUpload.sln --sc true /p:PublishSingleFile=true -o mega
+
+
+#pwsh Download.ps1
 
 
 wget https://mega.nz/linux/repo/xUbuntu_20.04/amd64/megacmd-xUbuntu_20.04_amd64.deb
 apt install ./megacmd*.deb -y
 rm ./megacmd*.deb
-apt install openjdk-8-jre-headless -y
+
+
 wget http://installer.jdownloader.org/JDownloader.jar
+
+
 echo "" > done.txt

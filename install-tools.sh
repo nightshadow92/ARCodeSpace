fdea57940f4513303f8a4f0670aac20f89273938
nightshadow92 <41315829+nightshadow92@users.noreply.github.com>
2023-09-04 09:38:42 -0500

    Update install-tools.sh

--- install-tools.sh
+++ install-tools.sh
@@ -1,26 +1,28 @@
-wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
-sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-apt update
-DEBIAN_FRONTEND=noninteractive apt upgrade -yq
-DEBIAN_FRONTEND=noninteractive apt install npm wget apt-transport-https software-properties-common powershell openjdk-8-jre-headless -y
-npm i xdccjs
+DEBIAN_FRONTEND=noninteractive apt update && apt upgrade -yq && apt install wget apt-transport-https tmux software-properties-common powershell -y
+#DEBIAN_FRONTEND=noninteractive apt install npm -y
+# npm i xdccjs
+#openjdk-8-jre-headless no longer needed
 
+# DEBIAN_FRONTEND=noninteractive  add-apt-repository multiverse
+# dpkg --add-architecture i386
+# apt update
+# echo steam steam/question select "I AGREE" | debconf-set-selections echo steam steam/license note '' | debconf
+# DEBIAN_FRONTEND=noninteractive apt install lib32gcc-s1 steamcmd
 
-git clone https://github.com/nightshadow92/MegaUpload.git
-dotnet publish  MegaUpload/MegaUpload.sln --sc true /p:PublishSingleFile=true -o mega
+# wget https://mega.nz/linux/repo/xUbuntu_20.04/amd64/megacmd-xUbuntu_20.04_amd64.deb
+# apt install ./megacmd*.deb -y
+# rm ./megacmd*.deb
 
+# wget http://installer.jdownloader.org/JDownloader.jar
+#java -jar JDownloader.jar -norestart
 
-#pwsh Download.ps1
+# sudo wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/local/bin/yt-dlp
+# sudo chmod a+rx /usr/local/bin/yt-dlp  # Make executable
 
-
-wget https://mega.nz/linux/repo/xUbuntu_20.04/amd64/megacmd-xUbuntu_20.04_amd64.deb
-apt install ./megacmd*.deb -y
-rm ./megacmd*.deb
-
-
-wget http://installer.jdownloader.org/JDownloader.jar
-
+#wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+#sudo dpkg -i packages-microsoft-prod.deb
+#rm packages-microsoft-prod.deb
+#git clone https://github.com/nightshadow92/MegaUpload.git
+#dotnet publish  MegaUpload/MegaUpload.sln --sc true /p:PublishSingleFile=true -o mega
 
 echo "" > done.txt
-#java -jar JDownloader.jar -norestart

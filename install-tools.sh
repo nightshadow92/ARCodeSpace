40ec6b733f9d3cf00b90a674f2c3cfc0c0bf2816
nightshadow92 <41315829+nightshadow92@users.noreply.github.com>
2023-12-16 16:01:30 +0000

    comment out install tools

--- install-tools.sh
+++ install-tools.sh
@@ -1,30 +1,30 @@
-DEBIAN_FRONTEND=noninteractive apt update && apt upgrade -yq && apt install wget apt-transport-https tmux software-properties-common powershell -y
-#DEBIAN_FRONTEND=noninteractive apt install npm -y
-# npm i xdccjs
-#openjdk-8-jre-headless no longer needed
+# DEBIAN_FRONTEND=noninteractive apt update && apt upgrade -yq && apt install wget apt-transport-https tmux software-properties-common powershell -y
+# #DEBIAN_FRONTEND=noninteractive apt install npm -y
+# # npm i xdccjs
+# #openjdk-8-jre-headless no longer needed
 
-# DEBIAN_FRONTEND=noninteractive  add-apt-repository multiverse
-# dpkg --add-architecture i386
-# apt update
-# echo steam steam/question select "I AGREE" | debconf-set-selections echo steam steam/license note '' | debconf
-# DEBIAN_FRONTEND=noninteractive apt install lib32gcc-s1 steamcmd
+# # DEBIAN_FRONTEND=noninteractive  add-apt-repository multiverse
+# # dpkg --add-architecture i386
+# # apt update
+# # echo steam steam/question select "I AGREE" | debconf-set-selections echo steam steam/license note '' | debconf
+# # DEBIAN_FRONTEND=noninteractive apt install lib32gcc-s1 steamcmd
 
-wget https://mega.nz/linux/repo/xUbuntu_20.04/amd64/megacmd-xUbuntu_20.04_amd64.deb
-apt install ./megacmd*.deb -y
-rm ./megacmd*.deb
+# wget https://mega.nz/linux/repo/xUbuntu_20.04/amd64/megacmd-xUbuntu_20.04_amd64.deb
+# apt install ./megacmd*.deb -y
+# rm ./megacmd*.deb
 
-mkdir jdownloader
-wget http://installer.jdownloader.org/JDownloader.jar -P ./jdownloader
-# java -jar JDownloader.jar -norestart
+# mkdir jdownloader
+# wget http://installer.jdownloader.org/JDownloader.jar -P ./jdownloader
+# # java -jar JDownloader.jar -norestart
 
-wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/local/bin/yt-dlp
-chmod a+rx /usr/local/bin/yt-dlp  # Make executable
-# apt install ffmpeg -y
+# wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/local/bin/yt-dlp
+# chmod a+rx /usr/local/bin/yt-dlp  # Make executable
+# # apt install ffmpeg -y
 
-#wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
-#sudo dpkg -i packages-microsoft-prod.deb
-#rm packages-microsoft-prod.deb
-#git clone https://github.com/nightshadow92/MegaUpload.git
-#dotnet publish  MegaUpload/MegaUpload.sln --sc true /p:PublishSingleFile=true -o mega
+# #wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+# #sudo dpkg -i packages-microsoft-prod.deb
+# #rm packages-microsoft-prod.deb
+# #git clone https://github.com/nightshadow92/MegaUpload.git
+# #dotnet publish  MegaUpload/MegaUpload.sln --sc true /p:PublishSingleFile=true -o mega
 
-echo "" > done.txt
+# echo "" > done.txt
